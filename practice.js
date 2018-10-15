a0d9e75225894c9f492efd63e3e891e37e3fc9a1
Pup <joshkelly.email@gmail.com>
2018-10-15 16:28:21 -0400

    Added images to gallery. Added music writing page.  Worked on style consistency across pages.

---
+++ practice.js
@@ -0,0 +1,13 @@
+	var one = 1;
+	var two = 2;
+
+	var firstName = "Josh";
+	var lastName = "Kelly";
+	var birthday = "March First 1992";
+
+	var interests = ["Music", "Writing", "Dogs", "Anime"];
+
+
+
+
+
